7af74479ac816d5a6725dd930248ed7dc169e1f7
Daniel Collin <daniel@collin.com>
2016-11-13 14:43:31 +0100

    WIP on ui_gen

--- src/tools/ui_gen/src/main.rs
+++ src/tools/ui_gen/src/main.rs
@@ -17,16 +17,38 @@ fn main() {
         e.get_kind() == EntityKind::StructDecl
     }).collect::<Vec<_>>();
 
+    println!("use std::os::raw::{{c_void, c_int, c_char}}\n");
+
     // Print information about the structs
     for struct_ in structs {
         let type_ =  struct_.get_type().unwrap();
         let size = type_.get_sizeof().unwrap();
-        println!("struct: {:?} (size: {} bytes)", struct_.get_name().unwrap(), size);
+        //println!("struct: {:?} (size: {} bytes)", struct_.get_name().unwrap(), size);
+    
+        if struct_.get_children().len() == 0 {
+            continue;
+        }
+        
+        println!("#[repr(C)]");
+        println!("pub struct {} {{", struct_.get_name().unwrap());
 
         for field in struct_.get_children() {
+            println!("{:?}", field);
             let name = field.get_name().unwrap();
-            let offset = type_.get_offsetof(&name).unwrap();
-            println!("    field: {:?} (offset: {} bits)", name, offset);
+
+            println!("--------------------------------------------");
+
+            for c in field.get_children() {
+                println!("c      {:?}", c);
+            }
+
+
+
+
+            //let offset = type_.get_offsetof(&name).unwrap();
+            println!("    pub {}", name);
         }
+
+        println!("}}\n");
     }
 }
