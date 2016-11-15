f2299692e33021337d965cfb8ec53a025daaf993
Daniel Collin <daniel@collin.com>
2016-11-15 11:56:39 +0100

    Implemented generation for Widget trait

---
+++ src/prodbg/wrui_rust/src/traits_gen.rs
@@ -0,0 +1,4 @@
+pub trait Widget {
+    fn set_size(&mut self, width: i32, height: i32),
+}
+
--- src/tools/ui_gen/src/main.rs
+++ src/tools/ui_gen/src/main.rs
@@ -1,12 +1,84 @@
+use std::io;
+use std::fs::File;
+use std::io::Write;
+
 extern crate clang;
 
 pub mod data;
 mod rust_ffi_gen;
 
+use data::*;
+
 use clang::*;
 
 static INPUT_HEADER: &'static str = "../../native/external/wrui/include/wrui.h";
-static RUST_FFI_FILE: &'static str = "../../../src/prodbg/wrui_rust/src/ffi.rs";
+static RUST_FFI_FILE: &'static str = "../../../src/prodbg/wrui_rust/src/ffi_gen.rs";
+static TRAITS_FILE: &'static str = "../../../src/prodbg/wrui_rust/src/traits_gen.rs";
+
+struct MatchName {
+	c_name: &'static str,
+	rust_name: &'static str,
+}
+
+pub fn generate_traits(filename: &str, structs: &Vec<Struct>) -> io::Result<()> {
+	let trait_names = [
+		MatchName {
+			c_name: "GUWidgetFuncs",
+			rust_name: "Widget",
+		}
+	];
+
+	let mut f = File::create(filename)?;
+
+	for struct_ in structs {
+		for trait_name in &trait_names {
+			println!("{} - {}", struct_.name, trait_name.c_name);
+			if struct_.name != trait_name.c_name {
+				continue;
+			}
+
+        	f.write_fmt(format_args!("pub trait {} {{\n", trait_name.rust_name))?;
+
+        	for entry in &struct_.entries {
+        		match entry {
+					&StructEntry::FunctionPtr(ref func_ptr) => {
+						f.write_fmt(format_args!("    fn {}(&mut self", func_ptr.name))?;
+
+						let arg_count = func_ptr.function_args.len();
+
+						for (i, arg) in func_ptr.function_args.iter().enumerate() {
+							// always skip first arg in trait (as its the C object)
+							if i == 0 {
+								f.write_all(b", ")?;
+								continue;
+							}
+
+							f.write_fmt(format_args!("{}: {}", arg.name, arg.rust_type))?;
+
+							if i != arg_count - 1 {
+								f.write_all(b", ")?;
+							}
+						}
+
+						f.write_all(b")")?;
+
+						if let Some(ref ret_var) = func_ptr.return_val {
+							f.write_fmt(format_args!(" -> {}", ret_var.rust_type))?;
+						}
+
+						f.write_all(b",\n")?;
+					}
+
+                	&StructEntry::Var(ref _var) => (),
+				}
+			}
+
+        	f.write_all(b"}\n\n")?;
+		}
+	}
+
+	Ok(())
+}
 
 fn main() {
     // Acquire an instance of `Clang`
@@ -32,6 +104,10 @@ fn main() {
     let structs = data::build_data(&structs);
 
     if let Err(err) = rust_ffi_gen::generate_ffi_bindings(RUST_FFI_FILE, &structs) {
+        panic!("Unable to generate {} err {:?}", RUST_FFI_FILE, err);
+    }
+
+    if let Err(err) = generate_traits(TRAITS_FILE, &structs) {
         panic!("Unable to generate {} err {:?}", RUST_FFI_FILE, err);
     }
 }
